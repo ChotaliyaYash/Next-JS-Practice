7c10d41301a6bbeb3b32523573e12d80ffec98a1
ChotaliyaYash <yashkumar.c@ahduni.edu.in>
2023-12-13 12:32:16 +0530

    Link component navigation

--- next_practice_one/src/app/page.tsx
+++ next_practice_one/src/app/page.tsx
@@ -1,11 +1,18 @@
 import React from 'react'
+import Link from 'next/link'
 
-const Home = () => {
+export default function Home() {
   return (
     <div>
         <h1>Route: /</h1>
+        <div className="flex flex-col gap-5">
+          <Link href={"/blog"}>Blogs</Link>
+          <Link href={"/login"}>Login</Link>
+          <Link href={"/register"}>Register</Link>
+          <Link href={"/products"}>Products</Link>
+          <Link href={"/profile"}>Profile</Link>
+          <Link href={"/about"}>About</Link>
+        </div>
     </div>
   )
 }
-
-export default Home
