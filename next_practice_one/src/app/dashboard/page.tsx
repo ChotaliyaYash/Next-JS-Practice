2720a51047cc6073f4cc48433aaf1f06147a3440
ChotaliyaYash <yashkumar.c@ahduni.edu.in>
2023-12-13 11:04:15 +0530

    file colocation means deep dive into file structure

---
+++ next_practice_one/src/app/dashboard/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import LineChart from '../../components/line-chart'
+
+const FunctionChart = () => {
+  return (
+    <div>
+        <h1>Charts</h1>
+        <LineChart />
+    </div>
+  )
+}
+
+export default FunctionChart
